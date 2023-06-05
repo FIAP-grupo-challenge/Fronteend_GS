bbd0ab23ebdce389a4b4c3843ebc383132d5465f
zzhyyy2004 <zzhyyy@protonmail.com>
2023-06-05 18:47:33 -0300

    mil coisas

--- dashboard/template_Luv_Painel_ADM/script.js
+++
@@ -1,9 +0,0 @@
-const url = 'http://127.0.0.1:80/api/get/all/text'; // Replace with your API endpoint
-
-fetch('https://example.com/api/data') // Replace with the URL of your API
-  .then(response => response.json())
-  .then(data => {
-    const text1 = document.getElementById('text_1');
-    text1.innerText = data["1"];
-  })
-  .catch(error => console.error(error));
