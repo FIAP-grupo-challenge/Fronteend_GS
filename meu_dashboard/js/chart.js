512e4680bebff49d8efcdaf34f475cafc958f498
zzhyyy2004 <zzhyyy@protonmail.com>
2023-06-06 12:41:00 -0300

    dash

---
+++ meu_dashboard/js/chart.js
@@ -0,0 +1,39 @@
+// Criação do gráfico de rendimento
+const ctx = document.getElementById('grafico').getContext('2d');
+
+    fetch('/api/rendimento')
+      .then(response => response.json())
+      .then(data => {
+        const meses = data.meses;
+        const rendimento = data.rendimento;
+
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: meses,
+            datasets: [{
+              label: 'Rendimento Mensal',
+              data: rendimento,
+              borderColor: 'green',
+              backgroundColor: 'rgba(0, 128, 0, 0.2)',
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'Rendimento (R$)'
+                }
+              },
+              x: {
+                title: {
+                  display: true,
+                  text: 'Mês'
+                }
+              }
+            }
+          }
+        });
+      });
